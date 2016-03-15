e4ba97d7db1204bd1a664852d094b097127e14a8
naxsel <asolanas93@gmail.com>
2016-03-15 11:46:28 +0100

    p3 v.01

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var server = require("./modules/server" );
-var router = require("./modules/router" );
-var requestHandlers = require("./modules/requestHandlers" );
+var server = require("./modules/server");
+var router = require("./modules/router");
+var requestHandlers = require("./modules/requestHandlers");
 var handle = {};
-handle["/" ] = requestHandlers.start;
-handle["/start" ] = requestHandlers.start;
-handle["/upload" ] = requestHandlers.upload;
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
 server.start(router.route, handle);
