de16bb65eb4b242cde4d5498aa6ad89488794694
naxsel <asolanas93@gmail.com>
2016-04-13 22:54:23 +0200

    muchas cosas

--- controllers/requestHandlers.js
+++ controllers/requestHandlers.js
@@ -1,6 +1,7 @@
 var querystring = require("querystring"),
     fs = require("fs"),
     formidable = require("formidable"),
+    url = require("url"),
     mysql = require("./mysql-connector");
 
 
@@ -36,7 +37,13 @@ function setMemo(response, request) {
     console.log("Request handler 'setMemo' was called.");
     var load = new formidable.IncomingForm();
     load.parse(req, function(err,fields,files){
-       
+        var file ="null";
+        if(files.fichero.name != ''){
+           fs.rename(files.fichero.path,PATH+files.fichero.name, function(error){
+           });
+        }else{
+
+        }
     });
     return;
     var aux = tabla;
@@ -50,6 +57,27 @@ function deleteMemo(response, request) {
 
 function showMemo(response, request){
     console.log("Request handler 'showMemo' was called.");
+    var aux = tabla;
+    var params = url.parse(request.url,true);
+    console.log(params);
+    mysql.FindByID(params.query.id,function(res){
+        console.log(res);
+        aux +='<tr><td><a class="btn btn-default" href="showMemo?id='+res[0].id+'">' +
+            '<span class="glyphicon glyphicon-search"></span></a></td>'+
+            '<td>"'+res[0].fecha+'"</td>'+
+            '<td>"'+res[0].texto+'"</td>';
+        if (res[0].fichero=="null"){
+            aux += '<td> No adjunto </td>';
+        }else{
+            aux += '<td><a href="'+PATH+res[0].fichero+'">'+res[0].fichero+'</a></td>';
+        }
+        aux+= '<td><a class = "btn btn-danger btn-xs" href="deleteMemo?id='+res[0].id+'">' +
+            '<span class="glyphicon glyphicon-trash"></span></td>';
+        console.log(aux);
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(aux);
+        response.end;
+    });
 
 }
 
@@ -64,9 +92,15 @@ var tabla  = '<!DOCTYPE html><html lang="en"><head><title>Gestor Tareas</title>'
     '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>'+
     '<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>'+
     '</head><body><div class="container"><h2>Gestor Tareas</h2><table class="table"><thead>'+
-    '<tr><th>Tarea</th><th>Fecha Limite</th><th>Comentarios</th><th>Fichero</th><th>Eliminar</th></tr></thead><tbody>'+
-    '<tr><td><a class="btn btn-default"><span class="glyphicon glyphicon-search"></span></a></td><td>John</td><td>Doe</td><td>john@example.com</td><td>' +
-    '<a class = "btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span></td></tr>';
+    '<tr><th>Tarea</th><th>Fecha Limite</th><th>Comentarios</th><th>Fichero</th><th>Eliminar</th></tr></thead><tbody>';
+
+var memo = '<!DOCTYPE html><html lang="en"><head><title>Tarea</title>'+
+    '<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">'+
+    '<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">'+
+    '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>'+
+    '<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>'+
+    '</head><body><div class="container"><h2>Gestor Tareas</h2><table class="table"><thead>'+
+    '<tr><th>Tarea</th><th>Fecha Limite</th><th>Comentarios</th><th>Fichero</th><th>Eliminar</th></tr></thead><tbody>';
 
 
 var form = '</tbody></table></br></br><h4>Añade una tarea</h4>' +
