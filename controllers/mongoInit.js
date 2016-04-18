a5d1dd12faeaa56bfe2e261783daec4c2f749ff7
naxsel <asolanas93@gmail.com>
2016-04-18 15:47:06 +0200

    mongo init

--- controllers/mongoInit.js
+++ controllers/mongoInit.js
@@ -5,11 +5,15 @@
  * Fecha: 17/4/2016
  * Funcion: Prepara la base de datos para el correcto funcionamiento del programa
  *          Crea las 2 colecciones e inserta unos datos de prueba
+ *          http://tphangout.com/how-to-encrypt-passwords-or-other-data-before-saving-it-in-mongodb/
  */
 
 
 'use strict';
 var MongoClient = require('mongodb').MongoClient;
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
+
 MongoClient.connect(
     'mongodb://127.0.0.1:27017/stw4',
     function(err, db) {
@@ -22,11 +26,20 @@ MongoClient.connect(
                     'texto': 'Prueba',
                     'fichero': 'nodejs-light.png'
                 }, function (err, res) {
-                    if (err) console.log(error);
+                    if (err) return next(err);
                     db.createCollection('users', function (err, collection) {
-                        collection.insert({'user': 'admin', 'password': 'test1'}, function (err, res) {
-                            if (err) console.log(error);
-                            db.close();
+                        var usuario = {'user': 'admin', 'password': 'test1'};
+                        bcrypt.genSalt(SALT_WORK_FACTOR, function(err,res) {
+                            if(err) return next(err);
+                            bcrypt.hash(usuario.password, salt, function(err,hash){
+                                if (err) return next(err);
+                                usuario.password = hash;
+                                collection.insert(usuario, function (err, res) {
+                                    if (err) console.log(error);
+                                    console.log("Datos creados, usar\n\tuser: admin\n\tpass: test1")
+                                    db.close();
+                                });
+                            });
                         });
                     });
                 });
