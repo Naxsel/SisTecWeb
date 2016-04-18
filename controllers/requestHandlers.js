33332f6e6a7e3b9be8820f2ea02b857b2d72f265
naxsel <asolanas93@gmail.com>
2016-04-18 22:51:36 +0200

    Text improved

--- controllers/requestHandlers.js
+++ controllers/requestHandlers.js
@@ -186,7 +186,7 @@ var tabla  = ''
 
 var memo = ''+
     '<!DOCTYPE html>' +
-    '<html lang="en"><head><title>Tarea</title>'+
+    '<html lang="en"><head><title>Gestor Tareas</title>'+
     '<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1">'+
     '<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">'+
     '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>'+
