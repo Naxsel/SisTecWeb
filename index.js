f632694dd108f6f36d7a7a09620a01164a116ee7
naxsel <asolanas93@gmail.com>
2016-04-20 21:08:43 +0200

    comments

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * Autor: Alejandro Solanas Bonilla
+ * NIA: 647647
+ * Fichero: index.js
+ * Fecha: 17/4/2016
+ * Funcion: Modulo inicial de la aplicacion
+ */
+
 var server = require("./controllers/server");
 var router = require("./controllers/router");
 var requestHandlers = require("./controllers/requestHandlers");
